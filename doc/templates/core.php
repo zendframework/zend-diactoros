5920139020726f42d1a92f742311380105887d20
Matthew Weier O'Phinney <matthew@zend.com>
2015-06-11 17:19:23 -0500

    Remove templates, CSS
    - Removed templates and CSS (moved to https://github.com/zendframework/documentation)

--- doc/templates/core.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo $this->render('navheader');
-echo $this->page->isIndex() ? $this->render('toc') : '';
-echo '<div class="row">' . $this->html . '</div>';
-echo $this->render('navfooter');
-?>
--- doc/templates/head.php
+++
@@ -1,7 +0,0 @@
-<head>
-    <title><?php echo $this->page->getTitle(); ?></title>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css" />
-    <link rel="stylesheet" href="http://uploads.mwop.net/prism-zf.css">
-    <link rel="stylesheet" href="css/docs.css" />
-</head>
