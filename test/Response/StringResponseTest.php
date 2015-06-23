3cfbfc143f6dd5ae6cf33bf1ae59f32eddc8038e
Franz Liedke <franz@develophp.org>
2015-06-23 14:36:17 +0200

    Remove trailing whitespace

--- test/Response/StringResponseTest.php
+++ test/Response/StringResponseTest.php
@@ -82,7 +82,7 @@ class StringResponseTest extends TestCase
         $this->assertEquals('application/json', $response->getHeaderLine('content-type'));
         $this->assertSame(json_encode([$value], JSON_UNESCAPED_SLASHES), (string) $response->getBody());
     }
-    
+
     public function testNoContentConstructor()
     {
         $response = StringResponse::noContent();
