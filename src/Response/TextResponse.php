3cabd6f5d120138e8c12603f95926be7ee0f7224
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-12-31 04:21:25 +0700

    Happy new year 2016

--- src/Response/TextResponse.php
+++ src/Response/TextResponse.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @see       http://github.com/zendframework/zend-diactoros for the canonical source repository
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2015-2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-diactoros/blob/master/LICENSE.md New BSD License
  */
 
