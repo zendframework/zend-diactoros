d831abb34e21b603b0d76bb8c1d3923c59d35b03
Matthew Weier O'Phinney <matthew@zend.com>
2016-10-11 08:09:32 -0500

    Added 421 response code ('Misdirected Request')
    Per @inoas

--- src/Response.php
+++ src/Response.php
@@ -75,6 +75,7 @@ class Response implements ResponseInterface
         416 => 'Requested range not satisfiable',
         417 => 'Expectation Failed',
         418 => 'I\'m a teapot',
+        421 => 'Misdirected Request',
         422 => 'Unprocessable Entity',
         423 => 'Locked',
         424 => 'Failed Dependency',
