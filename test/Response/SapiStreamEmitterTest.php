c90ca44ece17b6120b9c9d8d2560cf0b2a9d9662
Fábio Cabral Pacheco <fcabralpacheco@gmail.com>
2017-01-14 11:49:58 -0200

    Fix issue with high memory consumption in readable streams.

--- test/Response/SapiStreamEmitterTest.php
+++ test/Response/SapiStreamEmitterTest.php
@@ -10,6 +10,7 @@
 namespace ZendTest\Diactoros\Response;
 
 use PHPUnit_Framework_TestCase as TestCase;
+use Prophecy\Argument;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 use Zend\Diactoros\CallbackStream;
@@ -43,6 +44,7 @@ class SapiStreamEmitterTest extends SapiEmitterTest
         $stream = $this->prophesize('Psr\Http\Message\StreamInterface');
         $stream->__toString()->willReturn('Content!');
         $stream->isSeekable()->willReturn(false);
+        $stream->isReadable()->willReturn(false);
         $stream->eof()->willReturn(true);
         $stream->rewind()->willReturn(true);
         $stream->getSize()->willReturn(null);
@@ -56,6 +58,101 @@ class SapiStreamEmitterTest extends SapiEmitterTest
         foreach (HeaderStack::stack() as $header) {
             $this->assertNotContains('Content-Length:', $header);
         }
+    }
+
+    public function emitBodyProvider()
+    {
+        return [
+            [true,   '01234567890123456789'    , 10, 2],
+            [true,   '012345678901234567890123', 10, 3],
+            [false,  '01234567890123456789'    , 10, 2],
+            [false,  '012345678901234567890123', 10, 3],
+        ];
+    }
+
+    /**
+     * @dataProvider emitBodyProvider
+    */
+    public function testEmitBody($seekable, $contents, $maxBufferLength, $expectedReads = 0)
+    {
+        $position = 0;
+
+        $stream = $this->prophesize('Psr\Http\Message\StreamInterface');
+        $stream->getSize()->willReturn(strlen($contents));
+        $stream->isSeekable()->willReturn($seekable);
+        $stream->isReadable()->willReturn(true);
+        $stream->rewind()->willReturn(true);
+
+        $stream->eof()->will(function () use (&$contents, &$position){
+            return !isset($contents[$position]);
+        });
+
+        $stream->read(Argument::type('integer'))->will(function ($args) use (&$contents, &$position){
+                $data = substr($contents, $position, $args[0]);
+                $position += strlen($data);
+                return $data;
+            });
+
+        $response = (new Response())
+            ->withStatus(200)
+            ->withBody($stream->reveal());
+
+        ob_start();
+        $this->emitter->emit($response, $maxBufferLength);
+        $stream->rewind()->shouldBeCalledTimes($seekable ? 1 : 0);
+        $stream->read(Argument::type('integer'))->shouldBeCalledTimes($expectedReads);
+        $this->assertEquals($contents, ob_get_clean());
+
+    }
+
+    public function emitBodyRangeProvider()
+    {
+        return [
+            [true,  '01234567890123456789'    , ['bytes', 10, 20, '*'], 10, 1],
+            [true,  '012345678901234567890123', ['bytes', 10, 40, '*'], 10, 2],
+            [false, '01234567890123456789'    , ['bytes', 11, 20, '*'], 10, 1],
+            [false, '012345678901234567890123', ['bytes', 11, 40, '*'], 10, 2],
+        ];
+    }
+
+    /**
+     * @dataProvider emitBodyRangeProvider
+     */
+    public function testEmitBodyRange($seekable, $contents, $range, $maxBufferLength, $expectedReads = 0)
+    {
+        list($unit, $first, $last, $length) = $range;
+
+        $position = $first;
+
+        $stream = $this->prophesize('Psr\Http\Message\StreamInterface');
+        $stream->getSize()->willReturn(strlen($contents));
+        $stream->isSeekable()->willReturn($seekable);
+        $stream->isReadable()->willReturn(true);
+        $stream->seek(Argument::type('integer'))->will(function ($args) use (&$position){
+            $position = $args[0];
+            return true;
+        });
+
+        $stream->eof()->will(function () use (&$contents, &$position){
+            return !isset($contents[$position]);
+        });
+
+        $stream->read(Argument::type('integer'))->will(function ($args) use (&$contents, &$position){
+            $data = substr($contents, $position, $args[0]);
+            $position += strlen($data);
+            return $data;
+        });
+
+        $response = (new Response())
+            ->withStatus(200)
+            ->withHeader('Content-Range', "$unit $first-$last/$length")
+            ->withBody($stream->reveal());
+
+        ob_start();
+        $this->emitter->emit($response, $maxBufferLength);
+        $stream->seek(Argument::type('integer'))->shouldBeCalledTimes($seekable ? 1 : 0);
+        $stream->read(Argument::type('integer'))->shouldBeCalledTimes($expectedReads);
+        $this->assertEquals(substr($contents, $first, $last - $first + 1), ob_get_clean());
     }
 
     public function contentRangeProvider()
