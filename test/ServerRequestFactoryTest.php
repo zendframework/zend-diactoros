78372eab0d430c87a49814d15ab4d482f5119155
Witold Wasiczko <witold@wasiczko.pl>
2017-08-30 00:44:55 +0200

    Add test case

--- test/ServerRequestFactoryTest.php
+++ test/ServerRequestFactoryTest.php
@@ -98,6 +98,23 @@ class ServerRequestFactoryTest extends TestCase
         $this->assertEquals($expected, ServerRequestFactory::marshalHeaders($server));
     }
 
+    public function testRemoveVariablesPrefixedByApacheFromServerArrayIfNull()
+    {
+        // Non-prefixed versions will be preferred
+        $server = [
+            'HTTP_X_FOO_BAR' => null,
+            'REDIRECT_HTTP_AUTHORIZATION' => 'token',
+            'REDIRECT_HTTP_X_FOO_BAR' => 'prefixed',
+        ];
+
+        $expected = [
+            'authorization' => 'token',
+            'x-foo-bar' => 'prefixed',
+        ];
+
+        $this->assertEquals($expected, ServerRequestFactory::marshalHeaders($server));
+    }
+
     public function testStripQueryStringReturnsUnchangedStringIfNoQueryStringDetected()
     {
         $path = '/foo/bar';
@@ -468,6 +485,10 @@ class ServerRequestFactoryTest extends TestCase
                 'foo.bar=baz',
                 ['foo.bar' => 'baz'],
             ],
+            'empty' => [
+                '',
+                [],
+            ]
         ];
     }
 
