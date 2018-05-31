6ffa7f651075674b2e4495ebc47f9bda4b8851b0
Enrico Zimuel <e.zimuel@gmail.com>
2018-05-31 17:17:56 +0200

    Reduced the params conversion from Swoole to PSR-7

--- src/ServerRequestFactory.php
+++ src/ServerRequestFactory.php
@@ -119,44 +119,10 @@ abstract class ServerRequestFactory
         $cookie = isset($request->cookie) ? $request->cookie : [];
         $files  = isset($request->files) ? $request->files : [];
 
-        $server = [
-            'REQUEST_METHOD' => $request->server['request_method'],
-            'REQUEST_URI' => $request->server['request_uri'],
-            'PATH_INFO' => $request->server['path_info'],
-            'REQUEST_TIME' => $request->server['request_time'],
-            'GATEWAY_INTERFACE' => 'swoole/' . SWOOLE_VERSION,
-            // Server
-            'SERVER_PROTOCOL' => isset($request->header['server_protocol']) ?
-                                 $request->header['server_protocol'] : $request->server['server_protocol'],
-            'REQUEST_SCHEMA' => isset($request->header['request_scheme']) ?
-                                $request->header['request_scheme'] :
-                                explode('/', $request->server['server_protocol'])[0],
-            'SERVER_NAME' => isset($request->header['server_name']) ?
-                             $request->header['server_name'] : $request->header['host'],
-            'SERVER_ADDR' => $request->header['host'],
-            'SERVER_PORT' => isset($request->header['server_port']) ?
-                             $request->header['server_port'] : $request->server['server_port'],
-            'REMOTE_ADDR' => isset($request->server['remote_addr']) ?
-                             $request->server['remote_addr'] : $request->header['host'],
-            'REMOTE_PORT' => isset($request->header['remote_port']) ?
-                             $request->header['remote_port'] : $request->server['remote_port'],
-            'QUERY_STRING' => isset($request->server['query_string']) ?
-                              $request->server['query_string'] : '',
-            // Headers
-            'HTTP_HOST' => $request->header['host'],
-            'HTTP_USER_AGENT' => isset($request->header['user-agent']) ?
-                                 $request->header['user-agent'] : '',
-            'HTTP_ACCEPT' => isset($request->header['accept']) ?
-                             $request->header['accept'] : '*/*',
-            'HTTP_ACCEPT_LANGUAGE' => isset($request->header['accept-language']) ?
-                                      $request->header['accept-language'] : '',
-            'HTTP_ACCEPT_ENCODING' => isset($request->header['accept-encoding']) ?
-                                      $request->header['accept-encoding'] : '',
-            'HTTP_CONNECTION' => isset($request->header['connection']) ?
-                                 $request->header['connection'] : '',
-            'HTTP_CACHE_CONTROL' => isset($request->header['cache-control']) ?
-                                    $request->header['cache-control'] : '',
-        ];
+        $server = [];
+        foreach ($request->server as $key => $value) {
+            $server[strtoupper($key)] = $value;
+        }
 
         $headers = [];
         foreach ($request->header as $name => $value) {
