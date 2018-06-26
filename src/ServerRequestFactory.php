62974aac479d56a0135a5a157a1ae54c828e0046
Matthew Weier O'Phinney <matthew@zend.com>
2018-06-26 11:10:48 -0500

    Extracts ServerRequestFactory::parseCookieHeader to a function
    ADDS the function `Zend\Diactoros\parseCookieHeader()`.
    
    REMOVES the function `ServerRequestFactory::parseCookieHeader()`.

--- src/ServerRequestFactory.php
+++ src/ServerRequestFactory.php
@@ -82,7 +82,7 @@ abstract class ServerRequestFactory
         $headers = static::marshalHeaders($server);
 
         if (null === $cookies && array_key_exists('cookie', $headers)) {
-            $cookies = self::parseCookieHeader($headers['cookie']);
+            $cookies = parseCookieHeader($headers['cookie']);
         }
 
         return new ServerRequest(
@@ -418,35 +418,5 @@ abstract class ServerRequestFactory
             // Unset the port so the default port can be used
             $accumulator->port = null;
         }
-    }
-
-    /**
-     * Parse a cookie header according to RFC 6265.
-     *
-     * PHP will replace special characters in cookie names, which results in other cookies not being available due to
-     * overwriting. Thus, the server request should take the cookies from the request header instead.
-     *
-     * @param $cookieHeader
-     * @return array
-     */
-    private static function parseCookieHeader($cookieHeader)
-    {
-        preg_match_all('(
-            (?:^\\n?[ \t]*|;[ ])
-            (?P<name>[!#$%&\'*+-.0-9A-Z^_`a-z|~]+)
-            =
-            (?P<DQUOTE>"?)
-                (?P<value>[\x21\x23-\x2b\x2d-\x3a\x3c-\x5b\x5d-\x7e]*)
-            (?P=DQUOTE)
-            (?=\\n?[ \t]*$|;[ ])
-        )x', $cookieHeader, $matches, PREG_SET_ORDER);
-
-        $cookies = [];
-
-        foreach ($matches as $match) {
-            $cookies[$match['name']] = urldecode($match['value']);
-        }
-
-        return $cookies;
     }
 }
