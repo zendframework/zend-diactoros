f311faf560313b2b2a6fa53b68445a66d71b6582
Matthew Weier O'Phinney <matthew@zend.com>
2018-06-27 10:38:33 -0500

    Incorporates `normalizeUploadedFileSpecification()` into `normalizeUploadedFiles()`
    There's no need for `normalizeUploadedFileSpecification()` to be exposed
    publicly, as it is in internal detail of `normalizeUploadedFiles()`.

--- src/functions/normalize_uploaded_files.php
+++ src/functions/normalize_uploaded_files.php
@@ -22,6 +22,48 @@ use Psr\Http\Message\UploadedFileInterface;
  */
 function normalizeUploadedFiles(array $files)
 {
+    /**
+     * Normalize an array of file specifications.
+     *
+     * Loops through all nested files (as determined by receiving an array to the
+     * `tmp_name` key of a `$_FILES` specification) and returns a normalized array
+     * of UploadedFile instances.
+     *
+     * This function normalizes a `$_FILES` array representing a nested set of
+     * uploaded files as produced by the php-fpm SAPI, CGI SAPI, or mod_php
+     * SAPI.
+     *
+     * @param array $files
+     * @return UploadedFile[]
+     */
+    $normalizeUploadedFileSpecification = function (array $files = []) {
+        if (! isset($files['tmp_name']) || ! is_array($files['tmp_name'])
+            || ! isset($files['size']) || ! is_array($files['size'])
+            || ! isset($files['error']) || ! is_array($files['error'])
+        ) {
+            throw new InvalidArgumentException(sprintf(
+                '$files provided to %s MUST contain each of the keys "tmp_name",'
+                . ' "size", and "error", with each represented as an array;'
+                . ' one or more were missing or non-array values',
+                __FUNCTION__
+            ));
+        }
+
+
+        $normalized = [];
+        foreach (array_keys($files['tmp_name']) as $key) {
+            $spec = [
+                'tmp_name' => $files['tmp_name'][$key],
+                'size'     => $files['size'][$key],
+                'error'    => $files['error'][$key],
+                'name'     => isset($files['name'][$key]) ? $files['name'][$key] : null,
+                'type'     => isset($files['type'][$key]) ? $files['type'][$key] : null,
+            ];
+            $normalized[$key] = createUploadedFile($spec);
+        }
+        return $normalized;
+    };
+
     $normalized = [];
     foreach ($files as $key => $value) {
         if ($value instanceof UploadedFileInterface) {
@@ -30,7 +72,7 @@ function normalizeUploadedFiles(array $files)
         }
 
         if (is_array($value) && isset($value['tmp_name']) && is_array($value['tmp_name'])) {
-            $normalized[$key] = normalizeUploadedFileSpecification($value);
+            $normalized[$key] = $normalizeUploadedFileSpecification($value);
             continue;
         }
 
