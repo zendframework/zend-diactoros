6328ae39d9713c48bc5796be3f81f01046ec76bd
Matthew Weier O'Phinney <matthew@zend.com>
2018-09-05 13:18:17 -0500

    Incorporate feedback
    - Changes `$DEPENDENCIES` to `$INTEGRATION_DEPS` in `.travis.yml`, and
      changes conditional in `install` script to check for a non-empty value.
    - Updates license docblock in new files.

--- test/Integration/RequestTest.php
+++ test/Integration/RequestTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-diactoros for the canonical source repository
- * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-diactoros/blob/master/LICENSE.md New BSD License
  */
 
