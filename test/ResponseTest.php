5a7d1f829de87bd58ba7e88ad7645ad598e21857
webimpress <contact@webimpress.com>
2019-07-27 18:02:59 +0100

    Fixed failing tests: set user agent on load external content

--- test/ResponseTest.php
+++ test/ResponseTest.php
@@ -61,6 +61,7 @@ class ResponseTest extends TestCase
                     'http' => [
                         'method'  => 'GET',
                         'timeout' => 30,
+                        'user_agent' => 'PHP',
                     ],
                 ]
             )
