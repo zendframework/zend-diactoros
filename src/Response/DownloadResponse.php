071b3bd21650487ff5d260aadf0da58730d0a475
Matthew Setter <matthew@matthewsetter.com>
2019-12-31 10:38:01 +0100

    Split core of DownloadResponse back into a DownloadResponseTrait
    I think that I misunderstood what was being asked originally regarding
    working with a DownloadResponseTrait and a CsvResponse. Given that, this
    change is being made to create a DownloadResponse but keep the core of
    the functions in the trait. Not sure if this is the correct approach,
    but it makes sense to me at the moment.

--- src/Response/DownloadResponse.php
+++ src/Response/DownloadResponse.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-diactoros for the canonical source repository
- * @copyright Copyright (c) 2015-2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-diactoros/blob/master/LICENSE.md New BSD License
  */
 
@@ -12,12 +12,8 @@ namespace Zend\Diactoros\Response;
 use Psr\Http\Message\StreamInterface;
 use Zend\Diactoros\Exception\InvalidArgumentException;
 use Zend\Diactoros\Response;
-
 use Zend\Diactoros\Stream;
-use function array_keys;
-use function array_merge;
-use function implode;
-use function in_array;
+
 use function sprintf;
 
 /**
@@ -26,32 +22,10 @@ use function sprintf;
  */
 class DownloadResponse extends Response
 {
+    use DownloadResponseTrait;
+
     const DEFAULT_CONTENT_TYPE = 'application/octet-stream';
     const DEFAULT_DOWNLOAD_FILENAME = 'download';
-
-    /**
-     * A list of header keys required to be sent with a download response
-     *
-     * @var array
-     */
-    private $downloadResponseHeaders = [
-        'cache-control',
-        'content-description',
-        'content-disposition',
-        'content-transfer-encoding',
-        'expires',
-        'pragma'
-    ];
-
-    /**
-     * @var string The filename to be sent with the response
-     */
-    private $filename;
-
-    /**
-     * @var string The content type to be sent with the response
-     */
-    private $contentType;
 
     /**
      * DownloadResponse constructor.
@@ -75,81 +49,6 @@ class DownloadResponse extends Response
             $this->createBody($body),
             $status,
             $this->prepareDownloadHeaders($headers)
-        );
-    }
-
-    /**
-     * Get download headers
-     *
-     * @return array
-     */
-    private function getDownloadHeaders(): array
-    {
-        $headers = [];
-        $headers['cache-control'] = 'must-revalidate';
-        $headers['content-description'] = 'File Transfer';
-        $headers['content-disposition'] = sprintf('attachment; filename=%s', self::DEFAULT_DOWNLOAD_FILENAME);
-        $headers['content-transfer-encoding'] = 'Binary';
-        $headers['content-type'] = 'application/octet-stream';
-        $headers['expires'] = '0';
-        $headers['pragma'] = 'Public';
-
-        return $headers;
-    }
-
-    /**
-     * Check if the extra headers contain any of the download headers
-     *
-     * The download headers cannot be overridden.
-     *
-     * @param array $downloadHeaders
-     * @param array $headers
-     * @return bool
-     */
-    public function overridesDownloadHeaders(array $downloadHeaders, array $headers = []) : bool
-    {
-        $overridesDownloadHeaders = false;
-
-        foreach (array_keys($headers) as $header) {
-            if (in_array($header, $downloadHeaders)) {
-                $overridesDownloadHeaders = true;
-                break;
-            }
-        }
-
-        return $overridesDownloadHeaders;
-    }
-
-    /**
-     * Prepare download response headers
-     *
-     * This function prepares the download response headers. It does so by:
-     * - Merging the optional with over the default ones (the default ones cannot be overridden)
-     * - Set the content-type and content-disposition headers from $filename and $contentType passed
-     *   to the constructor.
-     *
-     * @param array $headers
-     * @return array
-     * @throws InvalidArgumentException if an attempt is made to override a default header
-     */
-    private function prepareDownloadHeaders(array $headers = []) : array
-    {
-        if ($this->overridesDownloadHeaders($this->downloadResponseHeaders, $headers)) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    'Cannot override download headers (%s) when download response is being sent',
-                    implode(', ', $this->downloadResponseHeaders)
-                )
-            );
-        }
-
-        return array_merge(
-            $headers,
-            $this->getDownloadHeaders(),
-            [
-                'content-disposition' => sprintf('attachment; filename=%s', $this->filename),
-                'content-type' => $this->contentType,
-            ]
         );
     }
 
